03e88d77d47e92b24d5d325b6a4d0fbbebb561b0
Billiam <billiamthesecond@gmail.com>
2023-08-05 11:17:05 -0500

    Disabe drag and drop, enable context menu in development

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,23 @@
-import { BrowserOpenURL } from '../wailsjs/runtime'
+import { BrowserOpenURL, Environment } from '../wailsjs/runtime'
 import {createApp} from 'vue'
 import App from './App.vue'
 
-document.body.addEventListener('contextmenu', e => e.preventDefault())
 createApp(App).mount('#app')
 
+const preventDefault = e => e.preventDefault()
+document.body.addEventListener('contextmenu', preventDefault)
+document.body.addEventListener('drop', e => {
+  e.preventDefault()
+})
+document.body.addEventListener('dragover', e => {
+  e.preventDefault()
+  e.stopPropagation()
+})
+Environment().then(({ buildType }) => {
+  if (buildType === 'dev') {
+    document.body.removeEventListener('contextmenu', preventDefault)
+  }
+})
 document.body.addEventListener('click', (event) => {
   if (event.target.nodeName === 'A') {
     BrowserOpenURL(event.target.getAttribute('href'))
